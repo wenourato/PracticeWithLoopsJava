06c2cd1f2af05d46c4c4a4b9751d30a014fb2213
Wyatt Enourato <wenourato@gmail.com>
2022-01-26 10:52:53 -0700

    Initial commit

---
+++ src/com/devmountain/loops/Main.java
@@ -0,0 +1,65 @@
+package com.devmountain.loops;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //for loop
+
+//	    for(int i = 0;  i < 5; i++)  //i < 5 means the amount of times the loop with itterate
+//            for(int i = 5; i > 0; i--)  // backwards for loop
+//            System.out.println("Hello World " + i);
+
+        //while loops
+//        int i = 0;
+//        while(i > 0){  //while loops are only better for when you dont know how many times you want to repeat something
+//            System.out.println("Hello World " + i);
+//            i--;
+////        }
+//        Scanner scanner = new Scanner(System.in);  //better to put our scanner outside the while loop so its not repeted
+//        String input = "";
+////        while (!input.equals("quit")) {
+////            System.out.print("Input: ");
+////            input = scanner.next().toLowerCase(); //this will return a string
+////            System.out.println(input);  //prints
+
+//        }
+
+        //do while loops will always run at least once, even if the condition is false
+
+
+//        do {
+//            System.out.print("Input: ");
+//            input = scanner.next().toLowerCase();
+//          System.out.println(input);
+//
+//        } while (!input.equals("quit"));
+
+//        Scanner scanner = new Scanner(System.in);
+//        String input = "";
+//        while (true) {  //this means that the method is true, and will contune to run until the user inputs "quit"
+//            System.out.print("Input: ");
+//            input = scanner.next().toLowerCase();
+//            if(input.equals("pass"))
+//                continue;  //this is telling the app to continue running, until it meets the break statement
+//            if (input.equals("quit"))
+//                break;  //this is telling the code to end after we type quit and ignore everything after
+//            System.out.println(input);
+//        }
+
+        //working with for each loops...REMEMBER they are forward only...cant loop backwards
+
+
+//        String[] fruits = {"Apple", "Mango", "Orange"};
+//
+//        for(int i = 0; i < fruits.length; i++) {
+//            System.out.println(fruits[i]);
+//        }
+//        for(String fruit: fruits) {
+//            System.out.println(fruit);
+//        }
+
+    }
+}
